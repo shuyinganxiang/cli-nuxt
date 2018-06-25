0fdd31567a9cc3620050c2713ce887ec8d92bff7
lichunluo <lichunluo92@163.com>
2018-06-26 00:22:56 +0800

    添加element ui 分页

---
+++ components/mains/content.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>content</h2>
+    <el-pagination
+      background="transparent"
+      border="1px solid #ccc"
+      :page-size="20"
+      :pager-count="11"
+      layout="prev, pager, next"
+      :total="100">
+    </el-pagination>
+  </div>
+</template>
--- pages/mains/index.vue
+++ pages/mains/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>  
     <left-nav class="left-nav" :isCollapse="isCollapse" />
-    <top-nav class="top-nav" :class="{ lnavhid: isCollapse }" @collapse="collapse">
-    </top-nav>
+    <top-nav class="top-nav" :class="{ lnavhid: isCollapse }" @collapse="collapse"></top-nav>
+    <contents class="contents" :class="{ lnavhid: isCollapse }"></contents>
       <!-- <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
         <el-radio-button :label="!isCollapse">展开</el-radio-button>
       </el-radio-group> -->
@@ -15,10 +15,12 @@
 <script>
   import leftNav from '../../components/common/leftNav'
   import topNav from '../../components/common/topNav'
+  import contents from '../../components/mains/content.vue'
   export default {
     components:{
       leftNav,
-      topNav
+      topNav,
+      contents
     },
     data() {
       return {
@@ -43,6 +45,11 @@
     /* float: left; */
     margin-left: 256px;
     background-color: deeppink;
+  }
+  .contents {
+    margin-left: 256px;
+    height: 300px;
+    background-color: greenyellow;
   }
   .lnavhid {
     margin-left: 80px;
